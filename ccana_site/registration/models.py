7f4960d1de35cd98243c820e1555e7daddf4abf5
ericpinter <ericpinter99@gmail.com>
2019-10-25 16:05:40 -0500

    Added a simple database and event creation
    I took out all of the test data I put in, so anyone who fetches should remember to migrate the database. events created at /registration/create/ are shown at /registration/

---
+++ ccana_site/registration/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=200)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=120)
+    start_time = models.DateTimeField('when the event starts')
+    end_time = models.DateTimeField('when the event ends')
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name+ " " + self.last_name
+
+class Attendance(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ ccana_site/registration/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Event
+from .forms import EventForm
+
+class IndexView(generic.ListView):
+    template_name = 'registration/index.html'
+    context_object_name = 'event_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Event.objects.order_by('-start_time').reverse()
+
+
+def create(request):
+    if (request.method=='POST'):
+        form = EventForm(request.POST)
+        if form.is_valid():
+            e = form.save(commit=False)
+            e.save();
+            return HttpResponseRedirect(reverse('registration:index'))
+    else:
+        print("create serving non-post");
+        form = EventForm();
+        return render(request, 'registration/create.html',{'form':form})
