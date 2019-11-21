fe0c683a840042502f27f32633015944e154bd80
zacharyanelson <nelsonza@hendrix.edu>
2019-11-20 18:23:52 -0600

    settign up auto send, urls and views

--- ccana_site/Contact-Us/views.py
+++ ccana_site/Contact-Us/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def contact(request);
+    return  HttpResponse('contact view')
