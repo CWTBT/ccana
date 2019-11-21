8cac5ca4646379894f98904c209343094904a0b6
zacharyanelson <nelsonza@hendrix.edu>
2019-11-21 10:27:24 -0600

    Settings.py has who to email to

--- ccana_site/Contact-Us/settings.py
+++ ccana_site/Contact-Us/settings.py
@@ -1,5 +1,10 @@
-INSTALLED_APPS [
-    'sendemail.apps.SendemailConfig', # new
-    ...
-]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+
+EMAIL_BACKEND =
+  'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
