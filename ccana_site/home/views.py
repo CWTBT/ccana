cc492ff9bd4131017d50258e2c000983e3e8596d
zacharyanelson <nelsonza@hendrix.edu>
2019-11-25 09:50:49 -0600

    EMail sett up

--- ccana_site/home/views.py
+++ ccana_site/home/views.py
@@ -21,6 +21,16 @@ def sponsors(request):
 
 def contact(request):
     form_class = ContactForm
+     if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+            , '')
+            contact_email = request.POST.get(
+                'contact_email'
+            , '')
+            form_content = request.POST.get('content', '')
+            email.send()
+            return redirect('contact')
 
     return render(request, 'contact.html', {
         'form': form_class,
