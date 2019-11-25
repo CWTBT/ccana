d385020e9654f6c725c7bea1ab7cec5b6902632a
zacharyanelson <nelsonza@hendrix.edu>
2019-11-25 09:16:18 -0600

    PUt my email as test email

--- ccana_site/Contact-Us/settings.py
+++ ccana_site/Contact-Us/settings.py
@@ -3,7 +3,7 @@
 
 EMAIL_BACKEND =
   'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'testing@example.com'
+DEFAULT_FROM_EMAIL = 'nelsonza@hendrix.edu'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
