007708e612471d223e31d98f9a2c0447e5c89baf
CWTBT <lw03232@gmail.com>
2019-12-15 15:38:15 -0600

    Beginning work on referral form

--- ccana_site/home/forms.py
+++ ccana_site/home/forms.py
@@ -5,3 +5,10 @@ class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
     content = forms.CharField(required=True, widget=forms.Textarea)
+
+class ReferralForm(forms.Form):
+    referral_name = forms.CharField(required=True)
+    referral_email = forms.EmailField(required=True)
+    from_time = forms.TimeField(required=True)
+    to_time = forms.TimeField(required=True)
+    needed_days = forms.ChoiceField(choices=["Monday-Friday", "Saturday-Sunday", "Other"], required=True)
