835041a8d8a064fafe27c6ffa839ec16edf6147b
CWTBT <lw03232@gmail.com>
2019-12-16 16:21:56 -0600

    Got choice fields working

--- ccana_site/home/forms.py
+++ ccana_site/home/forms.py
@@ -7,7 +7,9 @@ class ContactForm(forms.Form):
     content = forms.CharField(required=True, widget=forms.Textarea)
 
 class ReferralForm(forms.Form):
+    c = [("Licensed Center", "Licensed Center"), ("Licensed Family Home", "Licensed Family Home"), ("No Preference", "No Preference")]
     referral_name = forms.CharField(required=True)
     referral_email = forms.EmailField(required=True)
     from_time = forms.TimeField(required=True)
     to_time = forms.TimeField(required=True)
+    type = forms.ChoiceField(choices=c)
