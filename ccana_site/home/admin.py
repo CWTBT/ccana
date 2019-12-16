fc90bb7f6c1bcb3225326f83244304d3a4adb4cd
ericpinter <ericpinter99@gmail.com>
2019-12-16 16:44:30 -0600

    Reducing code reduplication

--- ccana_site/home/admin.py
+++ ccana_site/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from registration.models import Event, User
+from registration.models import Event, User, Attendance
 from django.http import HttpResponseRedirect
 
 admin.site.register(Event)
 admin.site.register(User)
+admin.site.register(Attendance)
