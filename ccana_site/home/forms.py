d3c9642749e50d61cdc4021a66b615ed0acec60b
CWTBT <lw03232@gmail.com>
2019-12-16 16:26:06 -0600

    Made choice fields required

--- ccana_site/home/forms.py
+++ ccana_site/home/forms.py
@@ -7,9 +7,11 @@ class ContactForm(forms.Form):
     content = forms.CharField(required=True, widget=forms.Textarea)
 
 class ReferralForm(forms.Form):
-    c = [("Licensed Center", "Licensed Center"), ("Licensed Family Home", "Licensed Family Home"), ("No Preference", "No Preference")]
+    type_c = [("Licensed Center", "Licensed Center"), ("Licensed Family Home", "Licensed Family Home"), ("No Preference", "No Preference")]
+    days_c = [("Monday - Friday","Monday - Friday"), ("Saturday - Sunday", "Saturday - Sunday")]
     referral_name = forms.CharField(required=True)
     referral_email = forms.EmailField(required=True)
     from_time = forms.TimeField(required=True)
     to_time = forms.TimeField(required=True)
-    type = forms.ChoiceField(choices=c)
+    type = forms.ChoiceField(choices=type_c, required=True)
+    days = forms.ChoiceField(choices=days_c, required=True)
