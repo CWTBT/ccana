ad1deb27842876d36ef1a844a8c071d2db14fd8b
CWTBT <lw03232@gmail.com>
2019-12-16 19:45:09 -0600

    Added better labels

--- ccana_site/home/forms.py
+++ ccana_site/home/forms.py
@@ -24,19 +24,19 @@ class ReferralForm(forms.Form):
         ("Stone", "Stone"),
         ("Van Buren", "Van Buren"),
         ("White", "White")]
-    referral_name = forms.CharField(required=True)
-    referral_email = forms.EmailField(required=True)
-    from_time = forms.TimeField(input_formats=('%I:%M %p',), required=True,
+    referral_name = forms.CharField(label="Name", required=True)
+    referral_email = forms.EmailField(label="Email", required=True)
+    from_time = forms.TimeField(label="From Time", input_formats=('%I:%M %p',), required=True,
         error_messages = {"invalid":"Please enter a time in the following format: HH:MM AM/PM"})
-    to_time = forms.TimeField(input_formats=('%I:%M %p',), required=True,
+    to_time = forms.TimeField(label="To Time", input_formats=('%I:%M %p',), required=True,
         error_messages = {"invalid":"Please enter a time in the following format: HH:MM AM/PM"})
-    type = forms.ChoiceField(choices=type_c, required=True)
-    days = forms.ChoiceField(choices=days_c, required=True)
+    type = forms.ChoiceField(label="Preferred Type of Care", choices=type_c, required=True)
+    days = forms.ChoiceField(label="Days Needed", choices=days_c, required=True)
     first_date = forms.DateField(label="First Child DOB (dd/mm/yyyy)", required=True)
     second_date = forms.DateField(label="Second Child DOB (dd/mm/yyyy)")
     third_date = forms.DateField(label="Third Child DOB (dd/mm/yyyy)")
     employer = forms.CharField(required=True)
-    spouse_employer = forms.CharField(required=True)
+    spouse_employer = forms.CharField(label="Spouse's Employer", required=True)
     address = forms.CharField(required=True)
     city = forms.CharField(required=True)
     state = forms.CharField(required=True)
